0f036b016ba232cb8890b8bbf3008f20a5a12dfc
Mario Sutalo <mario.sutalo@gmail.com>
2024-09-18 11:18:36 +0200

    -cp

---
+++ node crash course/test.js
@@ -0,0 +1,18 @@
+const firstName = 'Mario'
+const salary = 23
+const isSummer = true
+const item = {
+    name: 'PC',
+    price:1200
+}
+
+
+const greet = (firstName) => {
+    console.log(`Hi ${firstName}`)
+}
+
+function greet1(firstName) {
+    console.log(`Hi ${firstName}`)
+}
+greet('Mario')
+greet1('Yoshi')
