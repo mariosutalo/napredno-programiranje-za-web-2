de67c20d50ab0775cb86ffd69938874b7b8d5ca6
Mario Sutalo <mario.sutalo@gmail.com>
2024-09-30 12:20:35 +0200

    -cp

---
+++ node crash course/video 2/modules.js
@@ -0,0 +1,8 @@
+// const xyz = require('./people')
+const { people, ages } = require('./people')
+// console.log(xyz)
+// console.log(xyz.people, xyz.ages)
+console.log(people, ages)
+const os = require('os')
+// console.log(os)
+console.log(os.platform())
---
+++ node crash course/video 2/people.js
@@ -0,0 +1,4 @@
+const people = ['joshi', 'ryu', 'mario']
+const ages = [10, 20, 30, 40]
+// console.log(people)
+module.exports = { people, ages }
