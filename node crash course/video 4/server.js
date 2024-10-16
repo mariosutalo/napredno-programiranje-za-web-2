07a238b73b2e78a6b68c6646c15b6775ba58ab4d
Mario Sutalo <mario.sutalo@gmail.com>
2024-10-16 11:23:07 +0200

    -cp

--- node crash course/video 4/server.js
+++ node crash course/video 4/server.js
@@ -1,23 +1,38 @@
 const http = require('http')
 const fs = require('fs')
+const lodash = require('lodash')
+
 
 const server = http.createServer((req, res) => {
     console.log(`request url is: ${req.url}`)
     res.setHeader('Content-Type', 'text/html')
-
+    if(lodash.isString(234)){
+        console.log('value is string')
+    } else {
+        console.log('value is not string')
+    }
     let path = './views/'
     switch (req.url) {
         case '/':
             path += 'index.html'
+            res.statusCode = 200
             break;
         case '/about':
-            path = path + 'about'
+            path = path + 'about.html'
+            res.statusCode = 200
+            break
+        case '/about-me':
+            res.statusCode = 301
+            res.setHeader('Location','/about')
+            res.end()
             break
         case '/contact':
             path += 'contact.html'
+            res.statusCode = 200
             break
         default:
             path += '404.html'
+            res.statusCode = 404
             break;
     }
 
