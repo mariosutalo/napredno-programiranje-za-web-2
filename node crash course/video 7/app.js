cee252e28d6050fac8efdad134330d4b4e586db9
Mario Sutalo <mario.sutalo@gmail.com>
2024-10-28 13:24:23 +0100

    -cp

--- node crash course/video 7/app.js
+++ node crash course/video 7/app.js
@@ -2,23 +2,28 @@ const express = require('express')
 
 const app = express()
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
 app.listen(3000)
 
 app.get('/', (req, res) => {
-    res.render('index')
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum sit amet consecetur'},
+        {title: 'Mario finds eggs', snippet: 'Lorem ipsum sit amet consecetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum sit amet consecetur'}
+    ]
+    res.render('index', { title: 'Home', blogs})
 })
 app.get('/about', (req, res) => {
-    res.render('about')
+    res.render('about', { title: 'about' })
 })
 
-app.get('/blogs/create', (req,res) => {
-    res.render('create')
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'create blog' })
 })
 
 app.use((req, res) => {
-    res.status(404).render('404')
+    res.status(404).render('404', { title: 'page not found' })
 })
 
 console.log('end')
