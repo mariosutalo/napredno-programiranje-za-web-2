5bf00e7cd7caabc6ba80bcdbc1574d15949b1c15
Mario Sutalo <mario.sutalo@gmail.com>
2024-12-09 13:23:08 +0100

    -cp

---
+++ web shop/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.listen(3000)
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Products' })
+})
