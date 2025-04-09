333b508f43ddc1197ac2c421fd4567cb796c31bb
Mario Sutalo <mario.sutalo@gmail.com>
2025-04-09 11:24:42 +0200

    -cp

--- web-shop-admin/client/src/pages/editProduct/EditProduct.jsx
+++ web-shop-admin/client/src/pages/editProduct/EditProduct.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import useFetch from '../../hooks/useFetch'
+import styles from './EditProduct.module.css'
 
 const EditProduct = () => {
     const { id } = useParams()
@@ -10,7 +11,24 @@ const EditProduct = () => {
         <div>
             {error && <p>Error fetching</p>}
             {isPending && <p>Loading</p>}
-            {data && <p>{data.name}</p>}
+            {data && <form className={styles.product_form}>
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" name="name" value={data.name} />
+                <label htmlFor="price">Price:</label>
+                <input type="number" id="price" name="price" value={data.price} />
+                <label htmlFor="stock">Stock:</label>
+                <input type="number" id="stock" name="stock" value={data.stock}/>
+                <label htmlFor="category">Category:</label>
+                <input type="text" id="category" name="category" value={data.category_id}/>
+                <label htmlFor="specs">Specs:</label>
+                <textarea id="specs" name="specs" value={data.specs}/>
+                <label htmlFor="warranty">Warranty:</label>
+                <input type="number" id="warranty" name="warranty" value={data.warranty}/>
+                <label htmlFor="desc">Description:</label>
+                <textarea id="desc" name="desc" value={data.description}/>
+                <button type='submit'>Save changes</button>
+            </form>}
+            
         </div>
     )
 }
