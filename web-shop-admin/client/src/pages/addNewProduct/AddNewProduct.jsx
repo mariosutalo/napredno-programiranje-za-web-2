bf6782afeb6a98ea02d7c9071fbbda5a75600579
Mario Šutalo <mario.sutalo@gmail.com>
2025-09-30 08:55:54 +0200

    -cp

--- web-shop-admin/client/src/pages/addNewProduct/AddNewProduct.jsx
+++ web-shop-admin/client/src/pages/addNewProduct/AddNewProduct.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import ProductForm from '../../components/productForm/ProductForm'
 
 const getEmptyProductForm = () => {
